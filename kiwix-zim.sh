a26d79cfa3043b7f2f67cdd9f83e1d2d59406b75
DocDrydenn <docdrydenn@gmail.com>
2023-02-20 21:28:15 +0000

    Fix for ZIM's located in online OTHER directory.

--- kiwix-zim.sh
+++ kiwix-zim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VER="1.13"
+VER="1.15"
 
 # Set required packages Array
 PackagesArray=('curl')
@@ -161,11 +161,13 @@ flags() {
     for ((i=0; i<${#LocalZIMArray[@]}; i++)); do  # Loop through local ZIM(s).
         ZIMNameArray[$i]=$(basename "${LocalZIMArray[$i]}")  # Extract file name.
         IFS='_' read -ra fields <<< "${ZIMNameArray[$i]}"; unset IFS  # Break the filename into fields delimited by the underscore '_'
-
-        # *** Special Case for all STACKEXCHANGE ZIM's becasue they're too good to follow the naming standards. ***
         # First element is the Root - base directory for the URL
+        # *** Special Case for all STACKEXCHANGE ZIM's becasue they're too good to follow the naming standards. ***
         if [[ ${fields[0]} == *"stackexchange.com"* || ${fields[0]} == *"stackoverflow.com"* || ${fields[0]} == *"mathoverflow.net"* || ${fields[0]} == *"serverfault.com"* || ${fields[0]} == *"stackapps.com"* || ${fields[0]} == *"superuser.com"* || ${fields[0]} == *"askubuntu.com"* ]]; then
             ZIMRootArray[$i]="stack_exchange"
+        # *** Special Case for all ZIM's stored in the OTHER folder. ***
+        elif [[ ${fields[0]} == *"alittlequestionaday"* || ${fields[0]} == *"allthetropes"* || ${fields[0]} == *"alpinelinux"* || ${fields[0]} == *"appropedia"* || ${fields[0]} == *"archlinux"* || ${fields[0]} == *"artofproblemsolving"* || ${fields[0]} == *"bayardcuisine"* || ${fields[0]} == *"bitcoin"* || ${fields[0]} == *"bulbagarden"* || ${fields[0]} == *"chabadpedia"* || ${fields[0]} == *"crashcourse"* || ${fields[0]} == *"dandwiki"* || ${fields[0]} == *"diksha-std10ssc"* || ${fields[0]} == *"disledansmalangue"* || ${fields[0]} == *"ecured"* || ${fields[0]} == *"education-et-numerique"* || ${fields[0]} == *"edutechwiki"* || ${fields[0]} == *"ekopedia"* || ${fields[0]} == *"eleda"* || ${fields[0]} == *"energypedia"* || ${fields[0]} == *"eu4"* || ${fields[0]} == *"evageeks"* || ${fields[0]} == *"experiencesscientifiques"* || ${fields[0]} == *"explainxkcd"* || ${fields[0]} == *"finiki"* || ${fields[0]} == *"fountainpen"* || ${fields[0]} == *"gentoo"* || ${fields[0]} == *"granbluefantasy"* || ${fields[0]} == *"halachipedia"* || ${fields[0]} == *"hamichlol"* || ${fields[0]} == *"hitchwiki"* || ${fields[0]} == *"inciclopedia"* || ${fields[0]} == *"installgentoo"* || ${fields[0]} == *"jaimelire"* || ${fields[0]} == *"japprendsalire"* || ${fields[0]} == *"klexikon"* || ${fields[0]} == *"laboh"* || ${fields[0]} == *"les-fondamentaux"* || ${fields[0]} == *"lesbelleshistoires"* || ${fields[0]} == *"lesptitsphilosophes"* || ${fields[0]} == *"litterature-audiobooks-poetry"* || ${fields[0]} == *"los_miserables_audiobook"* || ${fields[0]} == *"mawsouaa"* || ${fields[0]} == *"mdwiki"* || ${fields[0]} == *"mesptitesquestions"* || ${fields[0]} == *"mesptitspourquoi"* || ${fields[0]} == *"metakgp"* || ${fields[0]} == *"mindfield"* || ${fields[0]} == *"neos-wiki"* || ${fields[0]} == *"openstreetmap-wiki"* || ${fields[0]} == *"physicell"* || ${fields[0]} == *"plume-app.co"* || ${fields[0]} == *"poesies"* || ${fields[0]} == *"pokepedia"* || ${fields[0]} == *"pokewiki"* || ${fields[0]} == *"rationalwiki"* || ${fields[0]} == *"scoopyendirectducorpshumain"* || ${fields[0]} == *"scratch-wiki"* || ${fields[0]} == *"skin-of-color-society"* || ${fields[0]} == *"storybox"* || ${fields[0]} == *"stupidedia"* || ${fields[0]} == *"t4-wiki"* || ${fields[0]} == *"termux"* || ${fields[0]} == *"thaki"* || ${fields[0]} == *"the_infosphere"* || ${fields[0]} == *"ubuntudoc"* || ${fields[0]} == *"ubuntuusers"* || ${fields[0]} == *"westeros"* || ${fields[0]} == *"whitewolfwiki"* || ${fields[0]} == *"wikem"* || ${fields[0]} == *"wikishia"* || ${fields[0]} == *"wikispecies"* || ${fields[0]} == *"wikisummaries"* || ${fields[0]} == *"wikiwel"* || ${fields[0]} == *"yeshiva"* || ${fields[0]} == *"youscribe"* || ${fields[0]} == *"zaya-english-duniya-marthi"* || ${fields[0]} == *"zdoom"* || ${fields[0]} == *"zimgit"* ]]; then
+            ZIMRootArray[$i]="other"
         else
             ZIMRootArray[$i]=${fields[0]} # All other non-stack_exchange ZIMs.
         fi
