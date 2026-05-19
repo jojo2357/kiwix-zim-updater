1c47c82ce20e0da4eebbb423a53fb4396f1fdb85
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2026-05-19 12:48:11 -0700

    bump version number

--- kiwix-zim-updater.sh
+++ kiwix-zim-updater.sh
@@ -5,7 +5,7 @@
 # Copyright (C) 2023  jojo2357
 # SPDX-License-Identifier: GPL-2.0-only
 
-VER="3.3"
+VER="3.4"
 
 # This array will contain all of the local zims, with the file extension
 LocalZIMArray=()
